935739ec10d153ea52c87851972705ab53706471
Eric Chiesse <techiesse@gmail.com>
2017-09-28 16:22:02 -0300

    Initial commit to najha.

---
+++ najha/functional.py
@@ -0,0 +1,123 @@
+# Function Generation
+def compose(f1, f2):
+    def ret(*args):
+        return f1(f2(*args))
+
+    return ret
+_c = compose
+
+
+def partial(f, *args1):
+    def ret(*args2):
+        args = args1 + args2
+        return f(*args)
+    return ret
+_p = partial
+
+
+# Error Handling:
+def pcall(f, *args, **kwargs):
+    ret = None
+    try:
+        ret = f(*args, **kwargs)
+    except:
+        pass
+
+    return ret
+
+
+# Higher order functions over lists:
+def all(f, iterable):
+    ret = True
+    for element in iterable:
+        if not f(element):
+            ret = False
+            break
+    return ret
+
+
+def any(f, iterable):
+    ret = False
+    for element in iterable:
+        if f(element):
+            ret = True
+            break
+    return ret
+
+
+_map = map
+def map(f, it):
+    return list(_map(f, it))
+
+
+_filter = filter
+def filter(f, it):
+    return list(_filter(f, it))
+
+
+# Funções sobre listas
+def take(n, container):
+    return container[:n]
+
+
+def drop(n, container):
+    return container[n:]
+
+
+def _advanceWhile(f, container):
+    n = 0
+    lim = len(container)
+    while n < lim and f(container[n]) == True:
+        n += 1
+    return n
+
+
+def takeWhile(f, container):
+    n = _advanceWhile(f, container)
+    return container[:n]
+
+
+def dropWhile(f, container):
+    n = _advanceWhile(f, container)
+    return container[n:]
+
+
+def head(container):
+    return container[0]
+
+
+def tail(container):
+    return container[1:]
+
+
+def cons(container, val):
+    ret = container[:]
+    ret.insert(0, val)
+    return ret
+
+
+# Misc
+def flip(f):
+    def ret(a, b):
+        return f(b, a)
+    return ret
+
+
+# Operadores:
+class op:
+    sum = lambda x, y: x + y
+    sub = lambda x, y: x - y
+    mul = lambda x, y: x * y
+    div = lambda x, y: x / y
+    eq = lambda x, y: x == y
+    neq = lambda x, y: x != y
+    gt = lambda x, y: x > y
+    ge = lambda x, y: x >= y
+    lt = lambda x, y: x < y
+    le = lambda x, y: x <= y
+    minus = lambda x: -x
+    andf = lambda x, y: x and y
+    orf = lambda x, y: x or y
+    notf = lambda x: not x
+    band = lambda x, y: x & y
+    bor = lambda x, y: x | y
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='najha',
+      version='0.1',
+      description='General purpose libraries for Python',
+      url='https://github.com/techiesse/najha',
+      author='Eric Chiesse',
+      author_email='techiesse@gmail.com',
+      license='MIT',
+      packages=['najha'],
+      zip_safe=False)

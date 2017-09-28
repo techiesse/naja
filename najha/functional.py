7b954bb3b570de62da4d1ef2b3504d91d9f54d2f
Eric Chiesse <techiesse@gmail.com>
2017-09-28 17:36:32 -0300

    Reordering and comments translated to english.

--- najha/functional.py
+++ najha/functional.py
@@ -1,61 +1,5 @@
-# Function Generation
-def compose(f1, f2):
-    def ret(*args):
-        return f1(f2(*args))
 
-    return ret
-_c = compose
-
-
-def partial(f, *args1):
-    def ret(*args2):
-        args = args1 + args2
-        return f(*args)
-    return ret
-_p = partial
-
-
-# Error Handling:
-def pcall(f, *args, **kwargs):
-    ret = None
-    try:
-        ret = f(*args, **kwargs)
-    except:
-        pass
-
-    return ret
-
-
-# Higher order functions over lists:
-def all(f, iterable):
-    ret = True
-    for element in iterable:
-        if not f(element):
-            ret = False
-            break
-    return ret
-
-
-def any(f, iterable):
-    ret = False
-    for element in iterable:
-        if f(element):
-            ret = True
-            break
-    return ret
-
-
-_map = map
-def map(f, it):
-    return list(_map(f, it))
-
-
-_filter = filter
-def filter(f, it):
-    return list(_filter(f, it))
-
-
-# Funções sobre listas
+# Functions over lists:
 def take(n, container):
     return container[:n]
 
@@ -96,6 +40,35 @@ def cons(container, val):
     return ret
 
 
+# Higher order functions over lists:
+def all(f, iterable):
+    ret = True
+    for element in iterable:
+        if not f(element):
+            ret = False
+            break
+    return ret
+
+
+def any(f, iterable):
+    ret = False
+    for element in iterable:
+        if f(element):
+            ret = True
+            break
+    return ret
+
+
+_map = map
+def map(f, it):
+    return list(_map(f, it))
+
+
+_filter = filter
+def filter(f, it):
+    return list(_filter(f, it))
+
+
 # Misc
 def flip(f):
     def ret(a, b):
@@ -103,7 +76,35 @@ def flip(f):
     return ret
 
 
-# Operadores:
+# Function Generation
+def compose(f1, f2):
+    def ret(*args):
+        return f1(f2(*args))
+
+    return ret
+_c = compose
+
+
+def partial(f, *args1):
+    def ret(*args2):
+        args = args1 + args2
+        return f(*args)
+    return ret
+_p = partial
+
+
+# Error Handling:
+def pcall(f, *args, **kwargs):
+    ret = None
+    try:
+        ret = f(*args, **kwargs)
+    except:
+        pass
+
+    return ret
+
+
+# Operators:
 class op:
     sum = lambda x, y: x + y
     sub = lambda x, y: x - y
